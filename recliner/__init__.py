aa60246292c55832c87113e8dc444b4b2ff6df04
Donald Stufft <donald.stufft@gmail.com>
2013-01-13 18:06:05 -0500

    Initial commit

---
+++ recliner/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+
+from recliner import __about__
+
+
+# - Meta Information -
+# This is pretty ugly
+for attr in __about__.__all__:
+    if hasattr(__about__, attr):
+        globals()[attr] = getattr(__about__, attr)
+# - End Meta Information -
---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+__about__ = {}
+
+with open("recliner/__about__.py") as fp:
+    exec(fp, None, __about__)
+
+setup(
+    name=__about__["__title__"],
+    version=__about__["__version__"],
+
+    description=__about__["__summary__"],
+    long_description=open("README.rst").read(),
+    url=__about__["__uri__"],
+    license=__about__["__license__"],
+
+    author=__about__["__author__"],
+    author_email=__about__["__email__"],
+
+    install_requires=[],
+    extras_require={
+        "tests": [
+            "pep8",
+            "pylint",
+            "pytest",
+            "pytest-cov",
+        ],
+    },
+    tests_require=[
+        "pep8",
+        "pytest",
+        "pytest-cov",
+    ],
+
+    packages=find_packages(exclude=["tests"]),
+
+    entry_points={
+        "console_scripts": [
+            "recliner = recliner.__main__:main",
+        ],
+    },
+
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+
+    zip_safe=False,
+)
